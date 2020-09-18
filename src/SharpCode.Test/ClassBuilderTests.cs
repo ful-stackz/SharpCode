315678d0e34c92295c8ec5752faf15d393a647e2
Ivan Stoyanov <ivan.stoyanov@esightmonitoring.nl>
2020-09-18 20:25:58 +0200

    Add more tests for ClassBuilder

--- src/SharpCode.Test/ClassBuilderTests.cs
+++ src/SharpCode.Test/ClassBuilderTests.cs
@@ -48,5 +48,57 @@ namespace Shapes
 
             Assert.AreEqual(expectedCode, generatedCode);
         }
+
+        [Test]
+        public void CreateClass_Works_WithFieldsAndProperties()
+        {
+            var generatedCode = Code.CreateClass("Triangle", AccessModifier.Public)
+                .WithNamespace("Shapes")
+                .WithField(Code.CreateField("double", "_hypotenuse"))
+                .WithField(Code.CreateField("double", "_adjacent"))
+                .WithField(Code.CreateField("double", "_opposite"))
+                .WithProperty(Code.CreateProperty("double", "Hypotenuse")
+                    .WithGetter("_hypotenuse")
+                    .WithSetter("_hypotenuse = value"))
+                .WithProperty(Code.CreateProperty("double", "Adjacent")
+                    .WithGetter("_adjacent")
+                    .WithSetter("_adjacent = value"))
+                .WithProperty(Code.CreateProperty("double", "Opposite")
+                    .WithGetter("_opposite")
+                    .WithSetter("_opposite = value"))
+                .ToSourceCode()
+                .Replace("\r\n", "\n");
+
+            string expectedCode = @"
+namespace Shapes
+{
+    public class Triangle
+    {
+        private double _hypotenuse;
+        private double _adjacent;
+        private double _opposite;
+        public double Hypotenuse
+        {
+            get => _hypotenuse;
+            set => _hypotenuse = value;
+        }
+
+        public double Adjacent
+        {
+            get => _adjacent;
+            set => _adjacent = value;
+        }
+
+        public double Opposite
+        {
+            get => _opposite;
+            set => _opposite = value;
+        }
+    }
+}
+            ".Trim();
+
+            Assert.AreEqual(expectedCode, generatedCode);
+        }
     }
 }
