8c1329b1b61967732d6210dfa2f8f8b4cbf9a6a1
Ivan Stoyanov <stoyanov.ivan98@gmail.com>
2020-09-20 17:49:54 +0200

    Make publishable
    - add packagable info in csproj
    - add pack and publish script
    - add pack and publish github action
    - add docs for releasing

---
+++ docs/releases.md
@@ -0,0 +1,28 @@
+# How to: Releases
+
+## Official releases
+
+Official releases are created through the designated GitHub action.
+
+1. Modify the [.version](../.version) file with the target version of the release
+2. Run the _"Pack and publish"_ GitHub action
+   - Provide the _"NuGet API key"_
+   - Set the _"Publish as beta"_ flag accordingly. Possible values are `$true` and `$false`
+3. Tag the released commit with the version from the .version file
+   - Use the `v{major.minor.patch[-beta]}` format
+
+## Local releases
+
+To make a local release/package you only need to run the `scripts/PackAndPublish.ps1` powershell script. It will then
+create a new folder `publish/` in the main directory with the nuget package inside.
+
+### Script arguments
+
+| Name | Type |Default | Description |
+| ---- | ---- | -------- | ----------- |
+| `Version` | `string` | `""` | The version that the package will have after being packaged. If not provided the conotents of the `.version` file will be used. |
+| `Beta` | `switch` | `false` | Appends `-beta` to the package version and changes the ID to `Beta.SharpCode` |
+
+### Examples
+- `PackAndPublish.ps1 -Version:1.0.0` will produce a package with version `1.0.0`
+- `PackAndPublish.ps1 -Version:1.0.0 -Beta` will produce a package with ID `Beta.SharpCode` and version `1.0.0-beta`
---
+++ scripts/PackAndPublish.ps1
@@ -0,0 +1,94 @@
+param (
+  [string] $Version,
+  [string] $Configuration="Release",
+  [string] $SourceDirectory="../src/SharpCode",
+  [string] $OutDirectory="../publish",
+  [string] $NuGetApiKey,
+  [switch] $Publish=$false,
+  [switch] $Beta=$false
+)
+
+function ReplaceInFile {
+  param (
+    [string] $filePath,
+    [string] $matchExpression,
+    [string] $replacedValue
+  )
+  ((Get-Content $filePath) -replace $matchExpression, $replacedValue) | Set-Content -Path $filePath
+}
+
+function AsSingleLine {
+  param ([string] $text)
+  # Remove line breaks, normalize excess spacing and trim
+  (($text -replace "\r?\n", "") -replace "\s+", " ").Trim()
+}
+
+function RunCmd {
+  param ([string] $command)
+  AsSingleLine $command | Invoke-Expression
+}
+
+if ($Version.Length -eq 0) {
+  $Version = Get-Content ../.version
+  if ($Version.Length -eq 0) {
+    Write-Error "Please specify the target version that the package will have when published"
+    exit 1
+  }
+}
+
+if ($Publish -and $NuGetApiKey.Length -eq 0) {
+  Write-Error "Please provide a NuGet API key for publishing the package"
+  exit 1
+}
+
+if ($Beta) {
+  $Version += "-beta"
+  ReplaceInFile `
+    -filePath:$SourceDirectory/SharpCode.csproj `
+    -matchExpression "<PackageId>SharpCode</PackageId>" `
+    -replacedValue "<PackageId>Beta.SharpCode</PackageId>"
+}
+
+Write-Host "Packing with configuration '$Configuration', version '$Version'..."
+RunCmd @"
+  dotnet pack
+  --configuration $Configuration
+  --output $OutDirectory
+  -p:version=$Version
+  $SourceDirectory
+"@
+
+if ($Beta) {
+  # Restore any ID changes after publishing to the beta feed
+  ReplaceInFile `
+    -filePath:$SourceDirectory/SharpCode.csproj `
+    -matchExpression "<PackageId>Beta.SharpCode</PackageId>" `
+    -replacedValue "<PackageId>SharpCode</PackageId>"
+}
+
+if ($LASTEXITCODE -ne 0) {
+  Write-Error -Message "An error occurred while creating package. dotnet pack exited with status $LASTEXITCODE"
+  exit $LASTEXITCODE
+} else {
+  Write-Host "Packing completed successfully" -ForegroundColor Green
+}
+
+if ($Publish) {
+  $FileName = "SharpCode.$Version.nupkg"
+  if ($Beta) { $FileName = "Beta.$FileName" }
+
+  Write-Host "Pushing package '$FileName' to NuGet..."
+  RunCmd @"
+    dotnet nuget push $OutDirectory/$FileName
+    --source https://api.nuget.org/v3/index.json
+    --api-key $NuGetApiKey
+    --no-symbols true
+"@
+  
+  if ($LASTEXITCODE -ne 0) {
+    Write-Error "An error occurred while pushing package to NuGet. dotnet nuget push exited with status $LASTEXITCODE"
+    exit $LASTEXITCODE
+  } else {
+    Write-Host "Package published successfully" -ForegroundColor Green
+  }
+}
